a62d0094816cb8beafeb8fd8678e95eb78c9d96a
ATN20222 <antonabdalla30@gmail.com>
2026-01-14 15:45:34 +0200

    change @ resend

--- api/contact.js
+++ api/contact.js
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
     const html = generateEmailHTML({ name, email, phone, subject, message })
 
     await resend.emails.send({
-      from: 'A.D.O CONSULT <onboarding@resend.dev>',
+      from: 'A.D.O CONSULT <ado@resend.dev>',
       to: ['info@ado-egy.com','antonabdalla30@gmail.com'],
       subject: `New Contact Form Submission: ${subject}`,
       reply_to: email,
